401f1624b2365400619b6490c92122e849744ff9
Gursimran27 <singhgursimran165@gmail.com>
2023-08-19 21:48:35 +0530

    19-8-23

---
+++ src/components/core/HomePage/Highlighttext.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Highlighttext = ( {text}) => {
+  return (
+    <span className='font-bold text-richblue-200'>
+        {" "}
+        {text}
+    </span>
+  )
+}
+
+export default Highlighttext
